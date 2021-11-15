3083429a0490ed2a65f8d5e63d777635cf97d42b
신 동준 <sindongjun@sins-MacBook-Pro.local>
2021-11-15 17:14:45 +0900

    feat: 스터디룸 새탭으로 입장 기능 추가, 스터디룸 중복입장 기능 추가, 스터디룸 입장 시 로그인 권한 추가

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -25,7 +25,7 @@ function App() {
           <Route path="/friend/" component={friend} />
           <Route path="/dm" component={dm} />
           <Route exact path="/test-StudyRoom" component={ Test_EnterStudyRoom } />
-          <Route path="/StudyRoom" component={ StudyRoom } />
+          <Route path="/StudyRoom/:studyRoomId/:nickName" component={StudyRoom} />
         </Switch>
       </BrowserRouter>
     </>
--- frontend/src/components/main/StudyCard.js
+++ frontend/src/components/main/StudyCard.js
@@ -1,33 +1,41 @@
-import React from "react";
-import './StudyCard.css'
-import styled from "styled-components";
+import React from 'react';
+import './StudyCard.css';
+import styled from 'styled-components';
 
-function StudyCard({title,imgUrl,body}) {
-    return(
+function StudyCard({ title, imgUrl, body, studyRoomID, nickName }) {
+    const isLogined = window.sessionStorage.userInfo == null ? false : true;
+
+    const enterStudyRoom = () => {
+        if (!isLogined) {
+            alert('로그인이 필요합니다.');
+            return;
+        }
+        if (window.localStorage.getItem('enteredStudyRoom') === 'true') alert('이미 스터디룸에 입장하였습니다.');
+        else {
+            window.localStorage.setItem('enteredStudyRoom', 'true');
+            window.open(`/StudyRoom/${studyRoomID}/${nickName}`, '_blank', 'noopener noreferrer');
+        }
+    };
+
+    return (
         <>
-            <button className="card-container">
+            <button className="card-container" onClick={enterStudyRoom}>
                 <div className="image-container">
-                    <img src ={imgUrl} alt='기본스터디이미지'/>
-
+                    <img src={imgUrl} alt="기본스터디이미지" />
                 </div>
                 <div className="card-content">
                     <div className="card-title">
                         <h3>{title}</h3>
                     </div>
                     <div className="card-body">
-                        <div className = "hashtagWrap">
-                            {body.map((x)=>{
-                                return (
-                                <>{'#'+x.hashtag+' '}</>
-                            )})}
+                        <div className="hashtagWrap">
+                            {body.map((x) => {
+                                return <>{'#' + x.hashtag + ' '}</>;
+                            })}
                         </div>
                     </div>
-
                 </div>
-
             </button>
-        
-        
         </>
     );
 }
